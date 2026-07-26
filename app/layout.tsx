5ab3d966d51f6a00c7b0e4767d2cc20d60481ff7
Alex Ngassaki <ngassakialex20@gmail.com>
2026-07-26 17:40:12 +0100

    ajout de la lib sonner pour les toasts

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import { Toaster } from "react-hot-toast";
+import { Toaster } from "@/components/ui/sonner";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -29,14 +29,13 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         {children}
-        <Toaster 
+        <Toaster
           position="top-center"
-          toastOptions={{
-            className: "",
-            style: {
-              marginTop: "10px",
-            }
-          }}
+           toastOptions={{
+              classNames: {
+                toast: "bg-black text-white border-zinc-800",
+              },
+            }}
         />
       </body>
     </html>
