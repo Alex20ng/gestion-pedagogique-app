b38f416823fc24afa6bf1a6cd0cadd5e4e05850f
papemd <papemd312@gmail.com>
2026-07-29 22:54:16 +0100

    Add faculty logo to classe interface

--- app/connexion/loginForm.tsx
+++ app/connexion/loginForm.tsx
@@ -21,7 +21,7 @@ export const LoginForm = ({action}: {action: any}) => {
 
         // TODO: remplacer par l'appel au nouveau backend, puis rediriger
         // selon le rôle renvoyé (etudiant / enseignant)
-        router.push("/etudiant/scanner");
+        router.push("/classe");
     }
 
     return (
